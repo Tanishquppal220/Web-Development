4a02b8ff698ce2bf1b2e29dff973a838b8a74cdb
Tanishq220 <Tanishquppal220@gmail.com>
2022-05-18 18:17:41 +0530

    Loops

---
+++ Loops/For_loops.js
@@ -0,0 +1,30 @@
+// * This to test the file
+// console.log("Hello world")
+
+// * This is a Example
+// let i = 10;
+
+// for (let i = 10; i > -1; i--) {console.log(i);}
+
+// * This is a Old method of loops for array
+let friends = ["A1","B1","C1"];
+
+for (let index = 0; index < friends.length; index++) {console.log ("Hello Friend, " + friends[index]);}
+
+// * This is Morden method of loops for array
+friends.forEach(function (element) {console.log("Hello Friend, " + element + " To Morden JS");})
+
+// * This is Newest method of loops for array
+for (element of friends) {console.log("Hello Friend, " + element + " To Newest JS");}
+
+// * This is for extarting info from an object
+
+let student = {
+  name: "Tanishq Uppal",
+  class: 11,
+  section: "A",
+};
+
+for (key in student) {
+    console.log(`The ${key} of the student is ${student[key]}` )
+}
---
+++ Loops/While_loops.js
@@ -0,0 +1,12 @@
+// * While Loop
+let i = 0;
+while (i < 4) {
+  console.log(`${i} is less than 4`);
+  i++;
+}
+// * do While Loop
+let j = 34;
+do {
+  console.log(`${j} is not less than 4`);
+  j++;
+} while (j < 4);
