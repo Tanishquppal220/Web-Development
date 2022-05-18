729d9db67dd3213c23349ffa43d070ce07f2ed76
Tanishq220 <Tanishquppal220@gmail.com>
2022-05-18 19:12:07 +0530

    Learnt Manuplating_DOM

---
+++ Manuplating_DOM/Manuplating_DOM.js
@@ -0,0 +1,13 @@
+// * This is for Manuplating DOM by ID
+// let nav = document.getElementById("nav")
+// console.log(nav)
+// let ul = document.getElementById("ul")
+// console.log(ul)
+
+// * This is for Manuplating DOM by CLASS
+// let a =document.getElementsByClassName("a")
+
+// * 
+let sel = document.querySelector(".a")
+console.log(sel)
+
